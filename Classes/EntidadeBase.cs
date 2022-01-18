5469885587c5efe509e560d80c2ca674ae48df72
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2022-01-17 22:07:33 -0300

    first commit

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,8 @@
+namespace Dio.Series // Note: actual namespace depends on the project name.
+{
+    public abstract class EntidadeBase
+    {
+       public int Id { get; protected set; }
+    }
+}    
+    
---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,39 @@
+// See https://aka.ms/new-console-template for more information
+
+using DIO.Series.Interfaces;
+
+namespace DIO.Series;
+
+public class SerieRepositorio : IRepositorio<Serie>
+{
+    private List<Serie> listaSerie = new List<Serie>();
+    public void Atualiza(int id, Serie objeto)
+    {
+        listaSerie[id] = objeto;
+    }
+
+    public void Exclui(int id)
+    {
+        listaSerie[id].Excluir();
+    }
+
+    public void Insere(Serie objeto)
+    {
+        listaSerie.Add(objeto);
+    }
+
+    public List<Serie> Lista()
+    {
+        return listaSerie;
+    }
+
+    public int ProximoId()
+    {
+        return listaSerie.Count;
+    }
+
+    public Serie RetornaPorId(int id)
+    {
+        return listaSerie[id];
+    }
+}
